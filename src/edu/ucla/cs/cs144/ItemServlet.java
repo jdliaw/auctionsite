f5139efb3181dab68a557b275761c14026297046
jdliaw <jennifer.liaw07@gmail.com>
2017-03-02 14:21:18 -0800

    working on itemservlet

--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -1,5 +1,10 @@
 package edu.ucla.cs.cs144;
 
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
 import java.io.IOException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -8,11 +13,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ItemServlet extends HttpServlet implements Servlet {
-       
-    public ItemServlet() {}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        // your codes here
+  public ItemServlet() {}
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+  {
+    String id = request.getParameter("id");
+    Element root = null;
+
+    String data = AuctionSearch.getXMLDataForItemId(id);
+    request.setAttribute("data", data);
+
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      Document doc = builder.parse(new InputSource(new StringReader(data)));
+      request.setAttribute("doc", doc);
+      root = doc.getDocumentElement();
     }
+    catch (Exception e) {
+      ;
+    }
+
+    request.setAttribute("test", root);
+
+    String name = MyParser.getElementTextByTagNameNR(root, "name");
+    request.setAttribute("name", name);
+
+    request.getRequestDispatcher("/item.jsp").forward(request, response);
+  }
 }
