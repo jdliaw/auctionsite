9d64b50d955f36c854dc9715b779fef5e24fa8b6
Brandon Liu <bliu7654@gmail.com>
2017-03-02 14:21:45 -0800

    commit

--- WebContents/search.jsp
+++ WebContents/search.jsp
@@ -8,15 +8,12 @@
 
   <body>
     <form>
-      <div>
-        <div>
-          <span>Enter a search query:</span><br/>
-          <input type="text" name="query">
-          <br>
-          <input id="submit" type="submit">
-          <h2>Searching for: ${query}</h2>
-        </div>
-      </div>
+      <span>Enter a search query:</span><br/>
+      <input type="text" name="query" value=${query}>
+      <br>
+      <input id="submit" type="submit" value="Search!">
+      <button onclick="skipResults()">More</button>
+      <h2>Searching for: ${query}</h2>
 
       <table>
         <c:forEach var="result" items="${results}">
@@ -27,11 +24,25 @@
         </c:forEach>
 
       </table>
-      <br>
+      <br> Print: ${print} <br>
+      <input name="numResultsToSkip" type="hidden" value="0">
+      <input name="numResultsToReturn" type="hidden" value="10">
 
-
+      <!--save data: http://stackoverflow.com/questions/1055308/retaining-the-submitted-jsp-form-data-->
 
     </form>
+
+    <script type="text/javascript">
+      function skipResults() {
+        var indexVal = document.getElementById("index").value;
+        var oldIndex = parseInt(indexVal);
+        var newIndex = oldIndex + 20;
+        var newIndexString = newIndex.toString();
+        document.getElementById("index").value = newIndexString;
+        console.log(document.getElementById("index").value);
+      }
+    </script>
   </body>
+
 
   </html>
--- src/edu/ucla/cs/cs144/SearchServlet.java
+++ src/edu/ucla/cs/cs144/SearchServlet.java
@@ -11,23 +11,46 @@ import java.io.PrintWriter;
 
 public class SearchServlet extends HttpServlet implements Servlet {
 
+    public static final int NUM_RESULTS_TO_DISPLAY = 20;
     public SearchServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        // get query
         String query = request.getParameter("query");
+        String numResultsToSkipString = request.getParameter("numResultsToSkip");
+        String numResultsToReturnString = request.getParameter("numResultsToReturn");
+        int numResultsToSkip;
+        int numResultsToReturn;
+        if(numResultsToSkipString == null) {
+            numResultsToSkip = 0;
+        }
+        else {
+            numResultsToSkip = Integer.parseInt(request.getParameter("numResultsToSkip"));
+        }
+
+        if(numResultsToReturnString == null) {
+            numResultsToReturn = 0;
+        }
+        else {
+            numResultsToReturn = Integer.parseInt(request.getParameter("numResultsToReturn"));
+        }
+
         if(query == null) {
             query = "";
         }
 
-        SearchResult[] results = AuctionSearch.basicSearch(query, 0, 20);
+
+
+
+        SearchResult[] results = AuctionSearch.basicSearch(query, numResultsToSkip, numResultsToReturn);
         if(results == null) {
             request.setAttribute("query", "It is a null value...");
         }
         else {
             request.setAttribute("results", results);
             request.setAttribute("query", query);
+            request.setAttribute("print", AuctionSearch.getXMLDataForItemId("1045272701"));
         }
 
         request.getRequestDispatcher("/search.jsp").forward(request, response);
