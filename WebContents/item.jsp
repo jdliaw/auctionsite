60da26214fa3d9c97f4edebc3d5709f9761ef087
jdliaw <jennifer.liaw07@gmail.com>
2017-03-02 15:01:14 -0800

    Parse xml doc and display name, description, categories

--- WebContents/item.jsp
+++ WebContents/item.jsp
@@ -1,12 +1,23 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
   <head>
     <title><%= request.getAttribute("name") %></title>
   </head>
   <body>
-    <p><%= request.getAttribute("data") %></p>
-    <h1>Name: <%= request.getAttribute("name") %></h1>
-
-    <h2>${doc}</h2>
-    <p>${test}</p>
+    <h1>${name}</h1>
+    <h4>Description</h4>
+    <p>${description}</p>
+    <table>
+      <tr>
+        <td><b>ID</b></td>
+        <td>${itemId}</td>
+      </tr>
+      <tr>
+        <td><b>Categories</b></td>
+        <c:forEach var="category" items="${categories}">
+          <td>${category}</td>
+        </c:forEach>
+      </tr>
+    </table>
   </body>
 </html>
--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 import org.xml.sax.*;
+import java.util.ArrayList;
 
 import java.io.IOException;
 import javax.servlet.Servlet;
@@ -22,23 +23,33 @@ public class ItemServlet extends HttpServlet implements Servlet {
     Element root = null;
 
     String data = AuctionSearch.getXMLDataForItemId(id);
-    request.setAttribute("data", data);
+    // request.setAttribute("data", data);
 
     try {
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       DocumentBuilder builder = factory.newDocumentBuilder();
       Document doc = builder.parse(new InputSource(new StringReader(data)));
-      request.setAttribute("doc", doc);
+      // request.setAttribute("doc", doc);
       root = doc.getDocumentElement();
     }
     catch (Exception e) {
-      ;
+      e.printStackTrace();
     }
 
-    request.setAttribute("test", root);
+    request.setAttribute("itemId", root.getAttribute("ItemID"));
 
-    String name = MyParser.getElementTextByTagNameNR(root, "name");
+    String name = MyParser.getElementTextByTagNameNR(root, "Name");
     request.setAttribute("name", name);
+
+    String description = MyParser.getElementTextByTagNameNR(root, "Description");
+    request.setAttribute("description", description);
+
+    ArrayList<String> categories = new ArrayList<String>();
+    Element[] catElem = MyParser.getElementsByTagNameNR(root, "Category");
+    for (int i = 0; i < catElem.length; i++) {
+      categories.add(MyParser.getElementText(catElem[i]));
+    }
+    request.setAttribute("categories", categories);
 
     request.getRequestDispatcher("/item.jsp").forward(request, response);
   }
